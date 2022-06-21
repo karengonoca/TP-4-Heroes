9d93984ef4c09b8fa60e4925d690ef638f93d236
Karen Gonzalez <kagoncaoca@gmail.com>
2022-06-20 23:33:15 -0300

    add styles

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.static('public'));
 
 
-app.get('/home', (req,res) => res.sendFile(path.join(__dirname, '/views/index.html')));
+app.get('/index', (req,res) => res.sendFile(path.join(__dirname, '/views/index.html')));
 app.get('/babbage', (req,res) => res.sendFile(path.join(__dirname, '/views/babbage.html')));
 app.get('/berners-lee', (req,res) => res.sendFile(path.join(__dirname, '/views/berners-lee.html')));
 app.get('/clarke', (req,res) => res.sendFile(path.join(__dirname, '/views/clarke.html')));
